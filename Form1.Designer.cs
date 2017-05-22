6d0bc733514a2f02e3bcd0dc16a63905bb7bb28e
ChauHuynh17071994 <hxtchau@gmail.com>
2017-05-22 11:34:07 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace DemoOCR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.rtb1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(42, 23);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(652, 307);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(42, 365);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(75, 23);
+            this.btnScan.TabIndex = 3;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(166, 364);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 4;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click_1);
+            // 
+            // rtb1
+            // 
+            this.rtb1.Location = new System.Drawing.Point(321, 365);
+            this.rtb1.Name = "rtb1";
+            this.rtb1.Size = new System.Drawing.Size(382, 188);
+            this.rtb1.TabIndex = 6;
+            this.rtb1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 565);
+            this.Controls.Add(this.rtb1);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.RichTextBox rtb1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+using Emgu.CV.OCR;
+//using tessnet2;
+namespace DemoOCR
+{
+    public partial class Form1 : Form
+    {
+        // variable
+        Capture capwebcam = null;
+        bool blnCapturingInprocess = false;
+        Image<Bgr, byte> imgOriginal;
+        private string m_path = Application.StartupPath + @"\data\";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_FormClose(object sender, FormClosedEventArgs e)
+        {
+            if (capwebcam != null)
+            {
+                capwebcam.Dispose();
+            }
+        }
+        void processFrameAndUpdateGUI(object sender, EventArgs arg)
+        {
+            imgOriginal = capwebcam.QueryFrame(); // get the next frame from the webcame
+            if (imgOriginal == null) return;     // if we did not get a frame.
+            Tesseract ocr = new Tesseract(@"C:\Emgu\emgucv-windows-x86 2.4.0.1717\bin\tessdata", "eng", Tesseract.OcrEngineMode.OEM_TESSERACT_ONLY);
+            ocr.Recognize(imgOriginal);
+            if (rtb1.Text != "") rtb1.AppendText(Environment.NewLine);
+            rtb1.AppendText(ocr.GetText());
+           
+
+            imageBox1.Image = imgOriginal;
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                capwebcam = new Capture();  // associate capture object to the default webcam
+            }
+            catch (NullReferenceException except)//catch error if unsucessful
+            {
+                rtb1.Text = except.Message;  // show error ohject message in text box
+                return;
+            }                               //Once we have a good capture object...
+
+            Application.Idle += processFrameAndUpdateGUI; // add process image function to the application's list of task
+            blnCapturingInprocess = true; //update member flag variable
+        }
+
+        private void btnPause_Click_1(object sender, EventArgs e)
+        {
+            if (blnCapturingInprocess == true)       // if we are currently, processing an image, user just chose pause to....
+            {
+                Application.Idle -= processFrameAndUpdateGUI;//remove the process image funtion from the application's list of tasks 
+                blnCapturingInprocess = false; // update flage variable
+                btnPause.Text = "Resume";      //update button text
+            }
+            else                                 // else if we are not currently processing an imge, user just chose resume to....
+            {
+                Application.Idle += processFrameAndUpdateGUI; //add the process image function to the application's list of tasks
+                blnCapturingInprocess = true;                   // update flag variable
+                btnPause.Text = "Pause";                        // now button will offer pause option
+            }
+        }
+    }
+}
